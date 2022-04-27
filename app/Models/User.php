5e209395ef72c293ebe97ac73a75ddd5a48676f8
Cyril Fehintoluwa <cyrilcril@gmail.com>
2022-04-27 05:44:22 +0100

    [added:relationship] User BelongsToMany Permission, User BelongsTo a Role(or has a Role)

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class)->withTimestamps();
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
