4725ae096f037556a5e000bdb8a7e9bf1bd9aa7e
Cyril Fehintoluwa <cyrilcril@gmail.com>
2022-04-28 17:25:28 +0100

    [added:Resource] UserResource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'full_name' => $this->fullname,
+            'email' => $this->email,
+            'permission' => $this->permission->name,
+            'created_date' => \Carbon\Carbon::parse($this->created_at)->timezone(config('timezone.timezone'))->isoFormat('MMM D, YYYY'),
+            'role' => $this->role->title
+        ];
+    }
+}
