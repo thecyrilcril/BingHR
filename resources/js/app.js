b15026aa44ac55dd1f0c7486f4d9859a629b55b7
Cyril Fehintoluwa <cyrilcril@gmail.com>
2022-04-28 06:55:36 +0100

    [risky] added vue to the project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-require('./bootstrap');
+import { createApp } from 'vue'
+import Index from './components/Index'
+
+const App = createApp({})
+App.component('index', Index)
+App.mount('#app')
---
+++ resources/js/components/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <p class="text-xl font-bold text-red-500">Welcome Home</p>
+</template>
+
+<script>
+export default {
+
+  name: 'Index',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
