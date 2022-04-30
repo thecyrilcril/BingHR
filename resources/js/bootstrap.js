b88db3d9b3f00b551b29da3cc3cbd584192104a8
Cyril Fehintoluwa <cyrilcril@gmail.com>
2022-04-30 08:44:01 +0100

    added interceptors to axios(redundant)

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,10 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.axios.interceptors.request.use(request => request)
+
+window.axios.interceptors.response.use(async response => response, async error => Promise.reject(error))
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
