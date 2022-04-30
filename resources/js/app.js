600b5bf505ced5032988d07f71dcd2551f4648da
Cyril Fehintoluwa <cyrilcril@gmail.com>
2022-04-30 08:47:15 +0100

    added vue-toastification to the project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,17 @@
+require('./bootstrap');
 import { createApp } from 'vue'
 import 'flowbite'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 import Index from './components/Index'
 
+const toastOptions = {
+    toastClassName: 'binghr',
+    bodyClassName: ['binghr'],
+}
 const App = createApp({})
 App.component('index', Index)
-App.mount('#app')
+App
+  .use(Toast, toastOptions)
+  .mount('#app')
+
